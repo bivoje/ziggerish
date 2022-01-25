eb9d841cb378e272a8131083d92a8a1290431c21
bivoje <bivoje49@gmail.com>
2022-01-25 14:37:34 +0000

    Enable compile using 'as' for 32bit linux

--- dull_rot25.c
+++ dull_rot25.c
@@ -1,8 +1,8 @@
 #include <syscall.h>
 #include <stdlib.h>
-char buf[1000];
-int intputchar(char *c);
-int intgetchar(char *c);
+static char buf[1000];
+static int intputchar(char *c);
+static int intgetchar(char *c);
 
 int main(void) {
   char *ptr = buf;
@@ -18,6 +18,7 @@ int main(void) {
   exit(0);
 }
 
+#ifdef BIT64
 int intputchar(char *c) {
   asm volatile (
     "syscall"
@@ -36,3 +37,24 @@ int intgetchar(char *c) {
   );
   if(ret!=1) *c=-1; // -1 == EOF
 }
+
+#else
+int intputchar(char *c) {
+  asm volatile (
+    "int $0x80"
+    :
+    : "a"(__NR_write), "b"(1), "c"(c), "d"(1)
+    : "memory"
+  );
+}
+int intgetchar(char *c) {
+  int ret;
+  asm volatile (
+    "int $0x80"
+    : "=a"(ret)
+    : "a"(__NR_read), "b"(0), "c"(c), "d"(1)
+    : "memory"
+  );
+  if(ret!=1) *c=-1; // -1 == EOF
+}
+#endif
--- src/main.zig
+++ src/main.zig
@@ -106,8 +106,6 @@ fn compile_as (
     options: CompileOptions,
 ) !void {
 
-    if (options.target != .linux_x86_64) { unreachable; }
-
     const asm_meta = //TODO
         \\	.file	"{s}"
         \\	.ident	"Sigmund: ({s} {any}) {any} {s}"
@@ -124,39 +122,98 @@ fn compile_as (
         \\
         \\	.text
         \\
-        \\intputchar:
-        \\	movl	$1, %eax
-        \\	movl	%eax, %edi
-        \\	syscall
-        \\	ret
-        \\
-        \\intgetchar:
-        \\	movl	$0, %eax
-        \\	movl	%eax, %edi
-        \\	syscall
-        \\	cmpl	$1, %eax
-        \\	je	.Lreadend
-        \\	movb	$-1, (%rsi)
-        \\.Lreadend:
-        \\	ret
-        \\
-        \\	.globl	_start
-        \\_start:
-        \\	pushq	%rbp
-        \\	subq	$8, %rsp
-        \\
-        \\	leaq	buf(%rip), %rsi
-        \\	movl	$1, %edx
-        \\
         ;
+    // NOTE gcc would normally generate code using .comm directive
+    // the difference is subtle, and negligible.
+    // https://stackoverflow.com/a/13584185
 
-    const asm_footer =
-        \\.Lexit:
-        \\	movl	$0, %edi
-        \\	movl	$60, %eax
-        \\	syscall
-        \\
-        ;
+    const asm_routines = switch (options.target) {
+        .linux_x86 =>
+            \\intputchar:
+            \\	movl	$4, %eax
+            \\	movl	$1, %ebx
+            \\	int	$0x80
+            \\	ret
+            \\
+            \\intgetchar:
+            \\	movl	$3, %eax
+            \\	movl	$0, %ebx
+            \\	int	$0x80
+            \\	cmpl	$1, %eax
+            \\	je	.Lreadend
+            \\	movb	$-1, (%ecx)
+            \\.Lreadend:
+            \\	ret
+            \\
+            ,
+        .linux_x86_64 =>
+            \\intputchar:
+            \\	movl	$1, %eax
+            \\	movl	%eax, %edi
+            \\	syscall
+            \\	ret
+            \\
+            \\intgetchar:
+            \\	movl	$0, %eax
+            \\	movl	%eax, %edi
+            \\	syscall
+            \\	cmpl	$1, %eax
+            \\	je	.Lreadend
+            \\	movb	$-1, (%rsi)
+            \\.Lreadend:
+            \\	ret
+            \\
+            ,
+        else => unreachable,
+    };
+
+    const asm_main_prol = switch (options.target) {
+        .linux_x86 =>
+            \\	.globl	_start
+            \\_start:
+            \\	pushl	%ebp
+            \\	subl	$8, %esp
+            \\
+            \\	leal	buf, %ecx
+            \\	movl	$1, %edx
+            \\
+            ,
+
+        .linux_x86_64 =>
+            \\	.globl	_start
+            \\_start:
+            \\	pushq	%rbp
+            \\	subq	$8, %rsp
+            \\
+            \\	leaq	buf(%rip), %rsi
+            \\	movl	$1, %edx
+            \\
+            , // FIXME use of %rip register in this code is placed by gcc
+              // in attempt to make PIE (position independent executable)
+              // thus, not needed in this case (except you want to load
+              // brackfuck code as shared library?)
+              // given -fno-pie option to gcc would prevent this behavior.
+              // https://stackoverflow.com/a/45422495
+        else => unreachable,
+    };
+
+    const asm_footer = switch (options.target) {
+        .linux_x86 =>
+            \\.Lexit:
+            \\	movl	$0, %ebx
+            \\	movl	$1, %eax
+            \\	int	$0x80
+            \\
+            ,
+        .linux_x86_64 =>
+            \\.Lexit:
+            \\	movl	$0, %edi
+            \\	movl	$60, %eax
+            \\	syscall
+            \\
+            ,
+        else => unreachable,
+    };
 
 
     // create *.s tempfile
@@ -191,6 +248,8 @@ fn compile_as (
 
         try file.writer().print(asm_meta, meta_info);
         try file.writeAll(asm_header);
+        try file.writeAll(asm_routines);
+        try file.writeAll(asm_main_prol);
 
         const jumprefs = (try collect_jumprefs(allocator, tokens)) orelse {
             return error.SytaxError;
@@ -198,31 +257,60 @@ fn compile_as (
 
         var jri: usize = 0;
         for (tokens.items) |token, i| {
-            try switch (token) {
-                .Left  => file.writeAll("\tsubq\t$1, %rsi"),     // <
-                .Right => file.writeAll("\taddq\t$1, %rsi"),     // >
-                .Inc   => file.writeAll("\taddb\t$1, (%rsi)"),  // +
-                .Dec   => file.writeAll("\tsubb\t$1, (%rsi)"),  // -
-                .Get   => file.writeAll("\tcall\tintgetchar"),   // ,
-                .Put   => file.writeAll("\tcall\tintputchar"),   // .
-                .From  => blk: { // [
-                    defer jri += 1;
-                    break :blk file.writer().print(
-                    \\.Lleft{d}:
-                    \\	movb	(%rsi), %al
-                    \\	testb	%al, %al
-                    \\	je .Lright{d}
-                    , .{i, jumprefs.items[jri]});
+            try switch (options.target) {
+                .linux_x86_64 => switch (token) {
+                    .Left  => file.writeAll("\tsubq\t$1, %rsi"),     // <
+                    .Right => file.writeAll("\taddq\t$1, %rsi"),     // >
+                    .Inc   => file.writeAll("\taddb\t$1, (%rsi)"),  // +
+                    .Dec   => file.writeAll("\tsubb\t$1, (%rsi)"),  // -
+                    .Get   => file.writeAll("\tcall\tintgetchar"),   // ,
+                    .Put   => file.writeAll("\tcall\tintputchar"),   // .
+                    .From  => blk: { // [
+                        defer jri += 1;
+                        break :blk file.writer().print(
+                        \\.Lleft{d}:
+                        \\	movb	(%rsi), %al
+                        \\	testb	%al, %al
+                        \\	je .Lright{d}
+                        , .{i, jumprefs.items[jri]});
+                    },
+                    .To    => blk: { // ]
+                        defer jri += 1;
+                        break :blk file.writer().print(
+                        \\.Lright{d}:
+                        \\	movb	(%rsi), %al
+                        \\	testb	%al, %al
+                        \\	jne	.Lleft{d}
+                        , .{i, jumprefs.items[jri]});
+                    },
                 },
-                .To    => blk: { // ]
-                    defer jri += 1;
-                    break :blk file.writer().print(
-                    \\.Lright{d}:
-                    \\	movb	(%rsi), %al
-                    \\	testb	%al, %al
-                    \\	jne	.Lleft{d}
-                    , .{i, jumprefs.items[jri]});
+                .linux_x86 => switch (token) {
+                    .Left  => file.writeAll("\tsubl\t$1, %ecx"),     // <
+                    .Right => file.writeAll("\taddl\t$1, %ecx"),     // >
+                    .Inc   => file.writeAll("\taddb\t$1, (%ecx)"),  // +
+                    .Dec   => file.writeAll("\tsubb\t$1, (%ecx)"),  // -
+                    .Get   => file.writeAll("\tcall\tintgetchar"),   // ,
+                    .Put   => file.writeAll("\tcall\tintputchar"),   // .
+                    .From  => blk: { // [
+                        defer jri += 1;
+                        break :blk file.writer().print(
+                        \\.Lleft{d}:
+                        \\	movb	(%ecx), %al
+                        \\	testb	%al, %al
+                        \\	je .Lright{d}
+                        , .{i, jumprefs.items[jri]});
+                    },
+                    .To    => blk: { // ]
+                        defer jri += 1;
+                        break :blk file.writer().print(
+                        \\.Lright{d}:
+                        \\	movb	(%ecx), %al
+                        \\	testb	%al, %al
+                        \\	jne	.Lleft{d}
+                        , .{i, jumprefs.items[jri]});
+                    },
                 },
+                else => unreachable,
             };
 
             try file.writer().writeByte(0x0a); // newline
@@ -238,6 +326,11 @@ fn compile_as (
             "as",
             "-o", options.compile_using.as.temp_path_o,
             options.compile_using.as.temp_path_s,
+            switch (options.target) {
+                .linux_x86 => "--32",
+                .linux_x86_64 => "--64",
+                else => unreachable,
+            },
             null,
         }, &[_:null]?[*:0]const u8{
             "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
@@ -251,6 +344,17 @@ fn compile_as (
         "ld",
         &[_:null]?[*:0]const u8{
             "ld",
+            // NOTE we need to explicitly specify target architecture
+            // for ld if it is different from current system archi.
+            // https://stackoverflow.com/a/16004418
+            "-m", switch (options.target) {
+                .linux_x86 => "elf_i386",
+                .linux_x86_64 => "elf_x86_64",
+                else => unreachable,
+            },
+            // NOTE -e <symbol> sets the entry point
+            // we can use other symbol than '_start'!
+            // https://sourceware.org/binutils/docs/ld/Entry-Point.html
             "-o", options.dst_path,
             options.compile_using.as.temp_path_o,
             null,
@@ -429,6 +533,12 @@ fn compile_gcc (
             "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
             null
         }
+        // NOTE to compile 32bit executable on 64bit system with gcc
+        // you would need gcc-multilib package installed.
+        // this is another dependency using gcc (with build-essentials)
+        // https://stackoverflow.com/a/46564864
+        // though, you cannot do so on Alpine Linux
+        // https://stackoverflow.com/a/40574830
     );
 
     if (ret.status != 0) return error.GccError;
